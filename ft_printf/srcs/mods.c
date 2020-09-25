5a9459eb99837f64c7d541930f50927ae76eb534
Leo Vintila <leovintila@gmail.com>
2020-09-25 22:25:09 +0200

    converts y mods desde macbookproLeo

--- ft_printf/srcs/mods.c
+++ ft_printf/srcs/mods.c
@@ -10,4 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+void	flags(const char *format, t_struct *f)
+{
+	while (ft_strchr("-+ #0", format[f->i]))
+	{
+		if (format[f->i] == '-')
+			f->minus = 1;
+		if (format[f->i] == '+')
+			f->plus = 1;
+		if (format[f->i] == ' ')
+			f->space = 1;
+		if (format[f->i] == '#')
+			f->hash = 1;
+		if (format[f->i] == '0')
+			f->zero = 1;
+		f->i++;
+	}
+	if (f->plus == 1)
+		f->space = 0;
+}
+
+void	width(const char *format, t_struct *f, va_list ap)
+{
+	width_star(format, f, ap);
+	if (format[f->i] >= '0' && format[f->i] <= '9')
+	{
+		f->width = ft_atoi(&format[f->i]);
+		while (format[f->i] >= '0' && format[f->i] <= '9')
+		{
+			
+		}
+	}
+}
