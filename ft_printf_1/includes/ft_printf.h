c20c3f0d9d5e2241440919da38adf5a446a5be63
blureddoor <46408741+blureddoor@users.noreply.github.com>
2020-10-01 19:23:53 +0200

    ft_ptintf_1

---
+++ ft_printf_1/includes/ft_printf.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lvintila <lvintila@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/28 19:31:09 by lvintila          #+#    #+#             */
+/*   Updated: 2020/09/30 20:01:43 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include "stdarg.h"
+# include "unistd.h"
+# include "limits.h"
+# include "stdlib.h"
+
+
+# define POSITIVE		66
+# define NEGATIVE		88
+
+# define CCONVERSIONS	"cspdiux X%"
+# define CFLAGS			"#-+ .*0"
+# define ALLSYMBOLS		"cspdiuxX %#-+ .*0"
+
+typedef struct	s_struct
+{
+	char		*format;
+	int			i;
+	int			nprinted;
+	int			minus;
+	int			zero;
+	int			width;
+	int			precision;
+}				t_struct;
+
+void			mods();
+void			converts(char c, va_list ap, t_struct *f);
+int				unsigned_nbr_len(uintmax_t n, int base);
+int				signed_nbr_len(intmax_t n, int base);
+char			*ft_itoa_base(uintmax_t n, char *base);
+char			*ft_itoa_base_upper(uintmax_t n, char *base);
+void			my_printf_nbr(va_list *my_list);
+void			my_printf_char(va_list *my_list);
+void			my_printf_str(va_list *my_lsit);
+int				my_printf(const char *src, ...);
+void			write_zeros(int n);
+void			write_blanks(int n);
+void			*ft_ultoa_base(unsigned long long n, char *base);
+
+#endif
---
+++ ft_printf_1/srcs/mods.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mods.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <@student.42madrid.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/30 19:48:46 by marvin            #+#    #+#             */
+/*   Updated: 2020/09/30 20:29:10 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	flags(const char *format, t_struct *f) // "-0"
+{
+	while(ft_strchr("-0"), format[f->i])
+	{
+		if (format[f->i] == '-')
+			f->minus = 1;
+		if (format[f->i] == '0')
+			f->zero = 1;
+		f->i++;
+	}
+}
+
+void	width(const char *format, t_struct *f, va_list ap)//  width
+{
+	width_star(format, f, ap);
+	if (format[f->i] >= '0' && format[f->i] <= '9')
+	{
+		f->width = ft_atoi(&format[f->i]);
+		while (format[f->i] >= '0' && foramt[f->i] <= '9')
+		{
+			f->i++;
+			if (format[f->i] == '*')
+			{
+				if (f->width > 0)
+				{
+					f->minus = 1;
+					f->width = -(f->width);
+				}
+				while (format[f->i] == '*')
+					f->i++;
+			}
+		}
+	}
+}
+
+void	precision(const char *format, t_struct *f, va_list ap, int n)// .
+{
+	int	i;
+
+	i = f->i;
+	if (format[i] == '.')
+	{
+		i++;
+		f->precision = 1;
+		if (format[i] >= '0' && format[i] <= '9')
+		{
+			f->precision = ft_atoi(&format[i]);
+			while (format[i] >= '0' && format[i] <= '9')
+				i++;
+		}
+		else if (format[f->i] == '*')
+		{
+			n = va_arg(ap, int);
+			if (n >= 0)
+				f->precision = n;
+			else if (n < 0)
+				f->precision = 0;
+			while (format[f->i] == '*')
+				i++;
+		}
+	}
+	f->i = i;
+}
+
+
+void	mods()
+{}
