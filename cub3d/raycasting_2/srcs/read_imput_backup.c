e207c8220ec63a53d8d10fe56a55dd2e9f4f5299
blureddoor <46408741+blureddoor@users.noreply.github.com>
2021-04-19 20:10:04 +0200

    FF

---
+++ cub3d/raycasting_2/srcs/read_imput_backup.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   read_imput.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/26 20:43:42 by marvin            #+#    #+#             */
+/*   Updated: 2021/04/05 20:56:08 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../raycasting/includes/cub.h"
+
+int 		check_tex(char *l, char *p)
+{
+	char *str;
+
+	str = ft_strmapi(l, space_tab);
+	if ((p = ft_strchr(str, 'R')) != NULL && *(p + 1) == ' ')
+	{
+//		ft_printf("2-checkpoint check_tex\n");
+		g_config.res = s_res(p);
+	}
+	else if ((p = ft_strnstr(str, "NO", ft_strlen(l))) != NULL && *(p + 2) == ' ')
+	{
+//		ft_printf("3-checkpoint chec_tex\n");
+		g_config.no = s_tex(p);
+	}
+	else if ((p = ft_strnstr(str, "SO", ft_strlen(l))) != NULL && *(p + 2) == ' ')
+	{
+		g_config.so = s_tex(p);
+//		ft_printf("4-checkpoint check_tex\n");
+	}
+	else if ((p = ft_strnstr(str, "EA", ft_strlen(l))) != NULL && *(p + 2) == ' ')
+	{
+		g_config.ea = s_tex(p);
+//		ft_printf("5-checkpoint check_tex\n");
+	}
+	else if ((p = ft_strnstr(str, "WE", ft_strlen(l))) != NULL && *(p + 2) == ' ')
+	{
+		g_config.we = s_tex(p);
+//		ft_printf("6-checkpoint check_tex\n");
+	}
+	else if ((p = ft_strchr(str, 'S')) != NULL && *(p + 1) == ' ')
+	{
+		g_config.s = s_tex(p);
+//		ft_printf("S-checkpoint check_tex\n");
+	}
+	else if ((p = ft_strchr(str, 'C')) != NULL && *(p + 1) == ' ' && ++g_config.fch)
+	{
+		g_config.floor = s_color(str, p);
+//		ft_printf("C-checkpoint check_tex\n");
+	}
+	else if ((p = ft_strchr(str, 'F')) != NULL && *(p + 1) == ' ' && ++g_config.cch)
+	{
+		g_config.ceiling = s_color(str, p);
+//		ft_printf("F-checkpoint check_tex\n");
+	}
+	else if (l[0])
+	{
+		return (1);
+//		ft_printf("7-checkpoint check_tex\n");
+	}
+	if (str)
+		free(str);
+	if (!l[0])
+	{
+		return (0);
+//		ft_printf("8-checkpoint check_tex\n");
+	}
+	g_config.count++;
+//	ft_printf("Fin-checkpoint check_tex\n");
+//	ft_printf("g_config.count = %d\n", g_config.count);
+	return (0);
+}
+
+int			read_config(char *argv)
+{
+	int fd;
+	char *line;
+	char *p;
+
+	p = NULL;
+	(((fd = open(argv, O_RDONLY)) <= 0) && (error(RED"- Can't open FD\n"RESET)));
+	while (g_config.count < NUM_FLAGS && get_next_line(fd, &line) > 0)
+	{
+//		ft_printf("1-checkpoint read_config\n");
+		(check_tex(line, p) && (error(RED"Wrong element in .cub\n"RESET)));
+        if (g_config.fch >= 2 || g_config.cch >= 2)
+            error(RED" FC is rep "RESET);
+		free(line);
+		line = NULL;
+	}
+//	ft_printf("2-checkpoint read_config\n");
+	read_map(fd);
+	if (line)
+		free(line);
+	close(fd);
+//	ft_printf("FINAL-read_config\n");
+	return (1);
+}
---
+++ cub3d/raycasting_2/srcs/texture.c
@@ -0,0 +1,114 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texture.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/16 19:56:58 by marvin            #+#    #+#             */
+/*   Updated: 2021/04/07 17:42:52 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../raycasting/includes/cub.h"
+
+int		side(t_game *game)
+{
+	int num;
+	((game->loop.ray_dir_x < 0 && game->loop.side == 0) && (num = 0));
+	((game->loop.ray_dir_x > 0 && game->loop.side == 0) && (num = 1));
+	((game->loop.ray_dir_y < 0 && game->loop.side == 1) && (num = 2));
+	((game->loop.ray_dir_y > 0 && game->loop.side == 1) && (num = 3));
+	return (num);
+}
+
+int		tex_calc(t_game *game)
+{
+	game->loop.texnum = side(game);
+	if (game->loop.side == 0)
+		game->loop.wall_x = game->loop.pos_y + game->loop.perpwalldist *
+			game->loop.ray_dir_y;
+	else
+		game->loop.wall_x = game->loop.pos_x + game->loop.perpwalldist *
+			game->loop.ray_dir_x;
+	game->loop.wall_x -= floor((game->loop.wall_x));
+	game->loop.tex_x = (int)(game->loop.wall_x * (double)
+			(game->texture->width));
+	if (game->loop.side == 0 && game->loop.ray_dir_x > 0)
+		game->loop.tex_x = game->texture->width -
+			game->loop.tex_x - 1;
+	if (game->loop.side == 1 && game->loop.ray_dir_y < 0)
+		game->loop.tex_x = game->texture->width - 
+			game->loop.tex_x - 1;
+	game->loop.step = 1.0 * game->texture->height /
+		game->loop.lineheight;
+	game->loop.tex_pos = (game->loop.draw_start - g_config.res.y / 2 +
+			game->loop.lineheight / 2) * game->loop.step;
+	return (0);
+}
+
+void		open_tex(t_game *game)
+{
+	game->texture[0].data = (int*)mlx_get_data_addr
+		(game->texture[0].img_ptr, &game->texture[0].bpp,
+		 &game->texture[0].size_l, &game->texture[0].endian);
+	game->texture[1].data = (int*)mlx_get_data_addr
+		(game->texture[1].img_ptr, &game->texture[1].bpp,
+		 &game->texture[1].size_l, &game->texture[1].endian);
+	game->texture[2].data = (int*)mlx_get_data_addr
+		(game->texture[2].img_ptr, &game->texture[2].bpp,
+		 &game->texture[2].size_l, &game->texture[2].endian);
+	game->texture[3].data = (int*)mlx_get_data_addr
+		(game->texture[3].img_ptr, &game->texture[3].bpp,
+		 &game->texture[3].size_l, &game->texture[3].endian);
+	game->texture[4].data = (int*)mlx_get_data_addr
+		(game->texture[4].img_ptr, &game->texture[4].bpp,
+		 &game->texture[4].size_l, &game->texture[4].endian);
+}
+
+char		*s_tex(char *line)
+{
+	char	**n_str;
+	char	*f_name;
+	
+	n_str = ft_split(line, ' ');
+	check_len(n_str, 2);
+	f_name = ft_strdup(n_str[1]);
+//	ft_printf("name[0] = %s\n", n_str[0]);
+//	ft_printf("name[1] = %s\n", n_str[1]);
+//	ft_printf("name[2] = %s\n", n_str[2]);
+	if (n_str[2])
+		error(RED" one of texture path it's not valid "RESET);
+	free_str(n_str);
+	return (f_name);
+}
+
+t_img		*init_texture(t_game *game)
+{
+	t_img	*texture;
+
+	texture = malloc(sizeof(t_img) * 5);
+	ft_bzero(texture, sizeof(t_img));
+	if (!(texture[0].img_ptr =
+				mlx_xpm_file_to_image(game->mlx.ptr, g_config.no,
+					&texture[0].width, &texture[0].height)))
+		error(RED"Can't open NO texture"RESET);
+	if (!(texture[1].img_ptr =
+				mlx_xpm_file_to_image(game->mlx.ptr, g_config.so,
+					&texture[1].width, &texture[1].height)))
+		error(RED"Can't open SO texture"RESET);
+	if (!(texture[2].img_ptr =
+				mlx_xpm_file_to_image(game->mlx.ptr, g_config.ea,
+					&texture[2].width, &texture[2].height)))
+		error(RED"Can't open EA texture"RESET);
+	if (!(texture[3].img_ptr =
+				mlx_xpm_file_to_image(game->mlx.ptr, g_config.we,
+					&texture[3].width, &texture[3].height)))
+		error(RED"Can't open WE texture"RESET);
+	if (!(texture[4].img_ptr =
+				mlx_xpm_file_to_image(game->mlx.ptr, g_config.s,
+					&texture[4].width, &texture[4].height)))
+		error(RED"Sprite Can't be opened"RESET);
+	return (texture);
+}
+
