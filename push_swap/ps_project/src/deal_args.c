5d29150cc1a9b1f2cc985a1cefeea77ff274299f
blureddoor <46408741+blureddoor@users.noreply.github.com>
2021-06-07 21:26:46 +0200

    few comment

--- push_swap/ps_project/src/deal_args.c
+++ push_swap/ps_project/src/deal_args.c
@@ -6,7 +6,7 @@
 /*   By: lvintila <lvintila@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/05 19:52:12 by lvintila          #+#    #+#             */
-/*   Updated: 2021/05/19 20:16:15 by marvin           ###   ########.fr       */
+/*   Updated: 2021/06/04 20:30:02 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,32 +28,6 @@ static int	split_args(char ***agg_split, int argc, char **argv)
 	}
 	free(agg);
 	agg = NULL;
-	return (1);
-}
-
-static int	check_args(char **agg_split, t_struct *ps)
-{
-	int	i;
-	int	nb_to_add;
-
-	nb_to_add = 0;
-	i = 0;
-	while (agg_split[i])
-	{
-		if (!check_line(ps, agg_split[i], &nb_to_add))
-		{
-			free(ps->stack_a.tab);
-			ps->stack_a.tab = NULL;
-			return (0);
-		}
-		if (!ft_realloc(&ps->stack_a, nb_to_add))
-		{
-			free(ps->stack_a.tab);
-			ps->stack_a.tab = NULL;
-			return (0);
-		}
-		i++;
-	}
 	return (1);
 }
 
@@ -105,6 +79,32 @@ int	ft_realloc(t_stack *stack, int nb_to_add)
 		free(stack->tab);
 	stack->tab = tmp;
 	stack->size++;
+	return (1);
+}
+
+static int	check_args(char **agg_split, t_struct *ps)
+{
+	int	i;
+	int	nb_to_add;
+
+	nb_to_add = 0;
+	i = 0;
+	while (agg_split[i])
+	{
+		if (!check_line(ps, agg_split[i], &nb_to_add))
+		{
+			free(ps->stack_a.tab);
+			ps->stack_a.tab = NULL;
+			return (0);
+		}
+		if (!ft_realloc(&ps->stack_a, nb_to_add))
+		{
+			free(ps->stack_a.tab);
+			ps->stack_a.tab = NULL;
+			return (0);
+		}
+		i++;
+	}
 	return (1);
 }
 
