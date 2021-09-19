553f0416037b720de9b19aa3f2884acd5809ee7c
Leo Vintila <leovintila@gmail.com>
2021-09-19 19:20:41 +0200

    pipex ok, from MBP

--- pipex_project/pipex_02/src/pipex.c
+++ pipex_project/pipex_02/src/pipex.c
@@ -15,42 +15,40 @@
 char	*find_path(char *cmd, char **envp)
 {
 	int i;
-	char **tab;
 	char *str;
+    char **tab;
 
 	if (access(cmd, F_OK) == 0)
-		return (cmd);
-	i = 0;
-	while (envp[i])
+        return (cmd);
+    i = 0;
+    while (envp[++i])
 	{
-		if (!ft_strncmp(envp[i], "PATH=", 5))
-		{
-			tab = ft_split(ft_strchr(envp[i], '/'), ':');
-			break;
-		}
-		i++;
-	}
-	i = 0;
-	while (tab[i])
-	{
-		str = ft_strjoin(ft_strjoin(tab[i], "/"), cmd);
-		if (access(str, F_OK) == 0)
-			return (str);
-		free(str);
-		i++;
-	}
-	return (cmd);
+	    if (!ft_strncmp(envp[i], "PATH=", 5))
+	    {
+		    tab = ft_split(ft_strchr(envp[i], '/'), ':');
+		    break;
+	    }
+    }
+    i = 0;
+    while (tab[++i])
+    {
+        str = ft_strjoin(ft_strjoin(tab[i], "/"), cmd);
+        if (access(str, F_OK) == 0)
+            return (str);
+    }
+    free(str);
+    free(tab);
+    return (cmd);
 }
 
 void	child1(int fd[2], int f1, char *cmd1, char **envp)
 {
-	int	end[2];
 	char **cmd;
 	char *str;
 
+	close(fd[0]);
 	dup2(f1, STDIN);
 	dup2(fd[1], STDOUT);
-	close(fd[0]);
 	close(f1);
 	cmd = ft_split(cmd1, ' ');
 	str = find_path(cmd[0], envp);
@@ -61,16 +59,15 @@ void	child1(int fd[2], int f1, char *cmd1, char **envp)
 
 void	parent1(int fd[2], int f2, char *cmd2, char **envp)
 {
-	int		end[2];
 	int		status;
 	char 	**cmd;
 	char 	*str;
 
+	close(fd[1]);
 	status = 0;
 	waitpid(-1, &status, 0);
 	dup2(f2, STDOUT);
 	dup2(fd[0], STDIN);
-	close(fd[1]);
 	close(f2);
 	cmd = ft_split(cmd2, ' ');
 	str = find_path(cmd[0], envp);
@@ -84,8 +81,8 @@ void	pipex(char *cmd1, char *cmd2, char **envp, char **argv)
 	int		status;
 	pid_t	parent;
 	int		fd[2];
-	int f1;
-	int	f2;
+	int     f1;
+	int	    f2;
 
 	pipe(fd);
 	parent = fork();
@@ -96,23 +93,14 @@ void	pipex(char *cmd1, char *cmd2, char **envp, char **argv)
 	if (parent < 0)
 		write(2, "Error\n", 6);
 	if (!parent)
-	{
-		close(fd[0]);
 		child1(fd, f1, cmd1, envp);
-	}
 	else
-	{
-		close(fd[1]);
 		parent1(fd, f2, cmd2, envp);
-	}
 }
-
 
 int	main(int argc, char **argv, char **envp)
 {
-	int f1;
-	int	f2;
-
 	pipex(argv[2], argv[3], envp, argv);
+    system("leaks pipex");
 	return (0);
 }
